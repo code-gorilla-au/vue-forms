5f743523555b5f0e5f7b6ecaff4c8197ada30b38
mauricio riquelme <m.riquelme223@gmail.com>
2023-03-23 09:25:37 +1100

    boilerplate

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,14 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: [
-    'prettier',
-    '@typescript-eslint/eslint-plugin',
-    'eslint-plugin-tsdoc',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  extends: ['plugin:@typescript-eslint/recommended'],
+  plugins: ['prettier', '@typescript-eslint/eslint-plugin', 'eslint-plugin-tsdoc'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/eslint-recommended'],
   parserOptions: {
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'prettier/prettier': ['error'],
+  },
   ignorePatterns: ['dist.*'],
-  overrides: [
-    {
-      files: '*.mjs',
-      env: {
-        node: true,
-        es6: true,
-      },
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-      },
-      plugins: [],
-      extends: 'eslint:recommended',
-    },
-    {
-      files: '*.vue',
-      extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', '@vue/typescript/recommended'],
-      globals: {
-        defineProps: 'readonly',
-        defineEmits: 'readonly',
-        defineExpose: 'readonly',
-        withDefaults: 'readonly',
-      },
-    },
-  ],
+  overrides: [],
 };
