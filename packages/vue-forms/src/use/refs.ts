9e9d9ec64e60ba4de9fbc67ce14d2137e2db302f
mauricio riquelme <m.riquelme223@gmail.com>
2023-03-31 16:26:14 +1100

    removing dead code

--- packages/vue-forms/src/use/refs.ts
+++ packages/vue-forms/src/use/refs.ts
@@ -15,12 +15,6 @@ export type MaybeElement =
 
 export type VueInstance = ComponentPublicInstance;
 
-export function unrefHasElement(elRef: MaybeElementRef) {
-  const rawEl = unref(elRef);
-  const el = (rawEl as VueInstance)?.$el ?? rawEl;
-  return el !== undefined;
-}
-
 export function resoleUnref<T>(ref: MaybeRef<T>): T {
   const rawEl = unref(ref);
   return (rawEl as VueInstance)?.$el ?? rawEl;
