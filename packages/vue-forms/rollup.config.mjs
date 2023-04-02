66f80a86e4081da5ceedef45c102ca5080c38c94
mauricio riquelme <m.riquelme223@gmail.com>
2023-04-03 09:21:45 +1000

    refactoring

--- packages/vue-forms/rollup.config.mjs
+++ packages/vue-forms/rollup.config.mjs
@@ -1,33 +1,29 @@
 import vue from 'rollup-plugin-vue';
-import typescript from 'rollup-plugin-typescript';
+import typescript from '@rollup/plugin-typescript';
 
 export default [
   {
-    input: './src/index.ts',
+    input: 'src/index.ts',
     output: {
       format: 'esm',
       file: 'dist/index.mjs',
     },
     plugins: [
       typescript({
-        tsconfig: false,
-        experimentalDecorators: true,
-        module: 'es2015',
+        tsconfig: 'tsconfig.json',
       }),
       vue(),
     ],
   },
   {
-    input: './src/index.ts',
+    input: 'src/index.ts',
     output: {
       format: 'cjs',
       file: 'dist/index.cjs',
     },
     plugins: [
       typescript({
-        tsconfig: false,
-        experimentalDecorators: true,
-        module: 'es2015',
+        tsconfig: 'tsconfig.json',
       }),
       vue(),
     ],
