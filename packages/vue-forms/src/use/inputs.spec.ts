325fd102d0ca0a7e6e568d7b3d832456972fee6e
mauricio riquelme <m.riquelme223@gmail.com>
2023-04-16 15:41:06 +1000

    update

--- packages/vue-forms/src/use/inputs.spec.ts
+++ packages/vue-forms/src/use/inputs.spec.ts
@@ -13,12 +13,41 @@ describe('useInputs()', () => {
     type: 'password',
     readOnly: false,
     required: false,
+    checked: false,
+    value: 'world',
     validity: {
       typeMismatch: false,
       valueMissing: false,
       valid: false,
     },
   } as HTMLInputElement;
+
+  class InputTestEvent extends EventTarget {
+    id: string;
+    name: string;
+    type: string;
+    readonly: boolean;
+    required: boolean;
+    checked: boolean;
+    value: string | boolean;
+    validity: {
+      typeMismatch: boolean;
+      valueMissing: boolean;
+      valid: boolean;
+    };
+
+    constructor(data: HTMLInputElement) {
+      super();
+      this.id = data.id;
+      this.name = data.name;
+      this.type = data.type;
+      this.readonly = data.readOnly;
+      this.required = data.required;
+      this.value = data.value;
+      this.checked = data.checked;
+      this.validity = data.validity;
+    }
+  }
 
   describe('core', () => {
     it('should return input state', async () => {
@@ -93,6 +122,119 @@ describe('useInputs()', () => {
 
       expect(wrapper.result.state.dirty).toBeTruthy();
       expect(wrapper.result.state.value).toEqual('foo');
+    });
+  });
+
+  describe('onInput', () => {
+    it('should update value for text', async () => {
+      const elRef = ref({
+        ...baseInput,
+        type: 'text',
+        value: 'world',
+      });
+      const wrapper = composableWrapper(() => {
+        const inputs = useInputs(elRef);
+        return inputs;
+      });
+
+      await flushPromises();
+
+      const testEvent = new InputTestEvent(elRef.value);
+      testEvent.addEventListener('custom', wrapper.result.onInput);
+      const ee = new CustomEvent('custom', {});
+      testEvent.dispatchEvent(ee);
+      await flushPromises();
+      expect(wrapper.result.state.value).toEqual('world');
+    });
+
+    it('should update value for password', async () => {
+      const elRef = ref({
+        ...baseInput,
+        type: 'password',
+        value: 'password',
+      });
+      const wrapper = composableWrapper(() => {
+        const inputs = useInputs(elRef);
+        return inputs;
+      });
+
+      await flushPromises();
+
+      const testEvent = new InputTestEvent(elRef.value);
+      testEvent.addEventListener('custom', wrapper.result.onInput);
+      const ee = new CustomEvent('custom', {});
+      testEvent.dispatchEvent(ee);
+      await flushPromises();
+      expect(wrapper.result.state.value).toEqual('password');
+    });
+
+    it('should update value for radio', async () => {
+      const elRef = ref({
+        ...baseInput,
+        type: 'password',
+        checked: true,
+        value: 'radio check',
+      });
+      const wrapper = composableWrapper(() => {
+        const inputs = useInputs(elRef);
+        return inputs;
+      });
+
+      await flushPromises();
+
+      const testEvent = new InputTestEvent(elRef.value);
+      testEvent.addEventListener('custom', wrapper.result.onInput);
+      const ee = new CustomEvent('custom', {});
+      testEvent.dispatchEvent(ee);
+      await flushPromises();
+      expect(wrapper.result.state.value).toEqual('radio check');
+    });
+
+    it('should update value for radio', async () => {
+      const elRef = ref({
+        ...baseInput,
+        type: 'checkbox',
+        checked: true,
+        value: 'should be true',
+      });
+      const wrapper = composableWrapper(() => {
+        const inputs = useInputs(elRef);
+        return inputs;
+      });
+
+      await flushPromises();
+
+      const testEvent = new InputTestEvent(elRef.value);
+      testEvent.addEventListener('custom', wrapper.result.onInput);
+      const ee = new CustomEvent('custom', {});
+      testEvent.dispatchEvent(ee);
+      await flushPromises();
+      expect(wrapper.result.state.value).toBeTruthy();
+    });
+
+    it('valid text should return valid', async () => {
+      const elRef = ref({
+        ...baseInput,
+        type: 'text',
+        value: 'should be valid',
+        validity: {
+          ...baseInput.validity,
+          valid: true,
+        },
+      });
+      const wrapper = composableWrapper(() => {
+        const inputs = useInputs(elRef);
+        return inputs;
+      });
+
+      await flushPromises();
+
+      const testEvent = new InputTestEvent(elRef.value);
+      testEvent.addEventListener('custom', wrapper.result.onInput);
+      const ee = new CustomEvent('custom', {});
+      testEvent.dispatchEvent(ee);
+      await flushPromises();
+      expect(wrapper.result.state.valid).toBeTruthy();
     });
   });
 });
