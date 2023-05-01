d32bd9080c839953977cf05e6864922c8b9eba9d
mauricio riquelme <m.riquelme223@gmail.com>
2023-05-01 21:33:17 +1000

    wip

--- examples/src/examples/vue-forms-ui.vue
+++ examples/src/examples/vue-forms-ui.vue
@@ -1,10 +1,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { UIInput, UICheckbox, UIForm } from '@code-gorilla-au/vue-forms-ui';
+import {
+  UIInput,
+  UICheckbox,
+  UIForm,
+  UIRadio,
+} from '@code-gorilla-au/vue-forms-ui';
 
 export default defineComponent({
   name: 'BasicForm',
-  components: { UIInput, UICheckbox, UIForm },
+  components: { UIInput, UICheckbox, UIForm, UIRadio },
   setup() {
     return {};
   },
@@ -12,21 +17,29 @@ export default defineComponent({
 </script>
 
 <template>
-  <UIForm #default="ddd">
+  <UIForm #default="formProps">
     <UIInput
-      class="my-3"
-      id="singleinput"
+      id="singleInput"
       name="singleInput"
       label="hello"
       type="email"
       required
     />
-    <UICheckbox class="my-3" id="checkboxInput" name="checkboxInput" required>
-      <p>hello</p>
+    <UICheckbox id="checkboxInput" name="checkboxInput" required>
+      <p>This is a checkbox</p>
     </UICheckbox>
+
+    <UIRadio
+      id="singleUiRadio"
+      name="singleUiRadio"
+      value="radio value"
+      required
+    >
+      This is a radio
+    </UIRadio>
     <pre>
       <code>
-        {{ ddd }}
+        {{ formProps }}
       </code>
     </pre>
   </UIForm>
---
+++ packages/vue-forms-ui/src/components/ui-radio.ts
@@ -0,0 +1,119 @@
+import { defineComponent, PropType, h, computed, ref } from 'vue';
+import { InputStyling } from '../lib/types';
+import { VInput } from '@code-gorilla-au/vue-forms';
+
+export default defineComponent({
+  name: 'UIRadio',
+  inheritAttrs: false,
+  emits: {
+    /**
+     * update model value
+     */
+    'update:modelValue': null,
+    /**
+     * input validation message
+     */
+    'update:validationMessage': null,
+  },
+  props: {
+    /**
+     * Id for input
+     */
+    id: {
+      type: String,
+      required: true,
+    },
+    /**
+     * name of the input
+     */
+    name: {
+      type: String,
+      required: true,
+    },
+    /**
+     * model value
+     */
+    modelValue: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+    value: {
+      type: String,
+      required: true,
+      default: undefined,
+    },
+    /**
+     * provide optional class styling
+     */
+    styling: {
+      type: Object as PropType<InputStyling>,
+      required: false,
+      default: () => ({}),
+    },
+  },
+  components: { VInput },
+  setup(props, ctx) {
+    const defaultSlot = computed(() => {
+      return ctx.slots.default && ctx.slots.default();
+    });
+
+    const validationMessage = ref('');
+
+    function updateValidationMessage(msg: string) {
+      validationMessage.value = msg;
+    }
+
+    const validationSlot = computed(() => {
+      if (validationMessage.value === '') {
+        return;
+      }
+      return h('p', {
+        innerText: validationMessage.value,
+        class: ['ui-radio-validation', props?.styling?.validation],
+      });
+    });
+
+    return () => {
+      return h(
+        'label',
+        {
+          class: ['', ctx.attrs.class],
+          for: props.id,
+        },
+        [
+          h(
+            'div',
+            {
+              class: ['ui-radio-container', props?.styling?.container],
+            },
+            [
+              h(
+                VInput,
+                {
+                  ...ctx.attrs,
+                  id: props.id,
+                  class: ['ui-radio-input', props?.styling?.input],
+                  type: 'radio',
+                  name: props.name,
+                  modelValue: props.modelValue,
+                  value: props.value,
+                  'onUpdate:validationMessage': updateValidationMessage,
+                },
+                {
+                  default() {
+                    return [
+                      h('span', { class: 'ui-radio-button' }),
+                      defaultSlot.value,
+                    ];
+                  },
+                },
+              ),
+            ],
+          ),
+          validationSlot.value,
+        ],
+      );
+    };
+  },
+});
