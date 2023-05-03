f838694556ebe2f83a5ca184f414409b7b5931f4
mauricio riquelme <m.riquelme223@gmail.com>
2023-05-04 09:34:38 +1000

    wip

--- packages/vue-forms/src/components/v-list.spec.ts
+++ packages/vue-forms/src/components/v-list.spec.ts
@@ -2,10 +2,26 @@
 
 import { render } from '@testing-library/vue';
 import VList from './v-list';
+import VInput from './v-input';
+import { defineComponent, h } from 'vue';
+
+const testComponent = defineComponent({
+  name: 'testComponent',
+  components: { VList, VInput },
+  setup() {
+    return () => {
+      return h('div', null, [
+        h(VList, { namespace: 'test' }, [
+          h(VInput, { id: '1', name: 'firstName' }),
+        ]),
+      ]);
+    };
+  },
+});
 
 describe('VList', () => {
   it('adding VList', () => {
-    const { html } = render(VList, {
+    const { html } = render(testComponent, {
       props: {
         namespace: 'list',
       },
